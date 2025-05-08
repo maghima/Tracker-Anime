d317e9a8dce72aa28c092707c090dbebacf3edd2
maghima <maghimat@gmail.com>
2025-05-08 23:51:35 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNdydbXC9OQGQ_4O6VfRCaBCpiL7jqAoc",
-  authDomain: "anime-tracker-6a427.firebaseapp.com",
-  projectId: "anime-tracker-6a427",
-  storageBucket: "anime-tracker-6a427.firebasestorage.app",
-  messagingSenderId: "418515259728",
-  appId: "1:418515259728:web:987cd479cf61d8eaec5c55"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
  
 const app = initializeApp(firebaseConfig);
